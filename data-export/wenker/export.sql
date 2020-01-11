b8b65ebc933c4c5ab6b491d9be9194da0314ab94
encima <chris@gwillia.ms>
2020-01-11 14:30:28 +0100

    updated wenker script

--- data-export/wenker/export.sql
+++ data-export/wenker/export.sql
@@ -1,5 +1,5 @@
 CREATE TEMP VIEW w1 AS
-	select submissions.created_at, u.username, u.email, submissions.content, t.id as TaskID, t.content->'question'->'text' as Question, a.name from submissions left join users u on submissions.user_id = u.id left join tasks t on submissions.task_id = t.id left join activities a on t.activity_id = a.id order by submissions.created_at DESC;
+	select submissions.created_at, u.username, u.email, STRING_AGG(item->>'text', ', ') as responses, t.id as TaskID, t.content->'question'->'text' as Question, a.name from submissions left join users u on submissions.user_id = u.id left join tasks t on submissions.task_id = t.id left join activities a on t.activity_id = a.id, jsonb_array_elements(submissions.content #> '{responses}' ) as item group by submissions.created_at, t.id, u.username, u.email, submissions.created_at, t.content->'question'->'text', a.name order by submissions.created_at DESC;
 
 \COPY (SELECT * FROM w1) TO './wenker_data.csv' WITH CSV DELIMITER ',' HEADER
 
